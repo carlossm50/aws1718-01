77d54bed4851a301757e59406095039b539975f8
aws1718casm <carlossm50@gmail.com>
2018-03-22 17:30:50 +0000

    Update for proyect

--- public/list-ctrl.js
+++ public/list-ctrl.js
@@ -10,6 +10,14 @@ angular
             });
         }
         
+        
+        $scope.GetContact = function (){
+            
+            $http.get("/api/v1/contacts/"+$scope.newContact.name).then(function (response){
+            $scope.contacts = response.data;
+            });
+        } 
+        
         $scope.addContact = function (){
             
             $http
@@ -17,9 +25,42 @@ angular
                 .then(function (){
                     refresh();  
                 });
-        
         }
         
+        $scope.delallContact = function (){
+            $http
+                .delete("/api/v1/contacts", $scope.delallContact)
+                .then(function (){
+                    refresh();  
+                });
+        }
+        
+        $scope.delContact = function (){
+            
+            $http
+                .delete("/api/v1/contacts/"+$scope.newContact.name , $scope.deleteContact)
+                .then(function (){
+                    refresh();  
+                });
+        }
+        
+        $scope.delContact = function (_name){
+            
+            $http
+                .delete("/api/v1/contacts/"+_name , $scope.deleteContact)
+                .then(function (){
+                    refresh();  
+                });
+        }
+        
+        $scope.updateContact = function (){
+            
+            $http
+                .put("/api/v1/contacts/"+$scope.newContact.name , {name:$scope.newContact.name,phone:$scope.newContact.phone,email:$scope.newContact.email})
+                .then(function (){
+                    refresh();  
+                });
+        }
         refresh();
         
     });
