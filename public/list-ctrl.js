30088d58cb2d09af5df2dc6651a7f44bc7626c75
aws1718pmb <paomarbau@alum.us.es>
2018-03-29 22:34:00 +0000

    update

--- public/list-ctrl.js
+++ public/list-ctrl.js
@@ -8,6 +8,7 @@ angular
 		$scope.grTrab = [];
         $scope.tareas = [];
 
+
         function refresh(){
             $http.get("/api/v1/projects").then(function (response){
                 $scope.projects = response.data;
@@ -62,6 +63,15 @@ angular
                 });
         }
         
+        $scope.updateProject1 = function (){
+            
+            $http
+                .put("/api/v1/projects/"+$scope.OneProject.projname , $scope.OneProject)//{projname:$scope.newProject.projname,tipo:$scope.newProject.tipo,referencia:$scope.newProject.referencia})
+                .then(function (){
+                    refresh();  
+                });
+        }
+        
         $scope.updateProject = function (){
             
             $http
@@ -73,38 +83,95 @@ angular
     
     //Inicio agregar y eliminar invertigador de la lista 
         
-		$scope.agregarTarea = function () {
-		if ($scope.nuevoInv != null)
-				$scope.tareas.push({inv_name: $scope.nuevoInv});
-				$scope.nuevoInv = null;
-                //alert($scope.tareas);
+		$scope.agregarTarea = function (_op) {
+
+    		if(_op === 0){
+    	    	if ($scope.nuevoInv != null)
+    				$scope.tareas.push({inv_name: $scope.nuevoInv});
+    			$scope.nuevoInv = null;
+    			
+    		}
+    		else{
+    		    if(_op === 1){
+    		        if ($scope.EditaInv != null)
+    				    $scope.OneProject.grInv.push({inv_name: $scope.EditaInv});
+    				$scope.EditaInv = null;
+    			    
+    		    }
+    		}
+		    
 		}
 
-		$scope.eliminarTarea = function (dato) {
+		$scope.eliminarTarea = function (_op,_index) {
 				//var pos = $scope.tareas.indexOf({texto:dato});
-				$scope.tareas.splice(dato);
+            if (_op === 0){
+				if (_index === 0 && $scope.tareas.length > 0){
+				    $scope.tareas.splice(0,1)
+				}
+				else{
+				    $scope.tareas.splice(_index,1)
+				}
+            }
+            else{
+                if (_op === 1){
+                    
+                    if (_index === 0 && $scope.OneProject.grInv.length > 0){
+    				    $scope.OneProject.grInv.splice(0,1)
+    				}
+    				else{
+    				    $scope.OneProject.grInv.splice(_index,1)
+    				    
+    				}
+                }
+            }
 		}
     
     
     //Inicio agregar y eliminar Personal de equipo de tabajo de la lista 
         
-		$scope.agregarPtrabajo = function () {
-		if ($scope.nuevoTrb != null)
-				$scope.grTrab.push({trb_name: $scope.nuevoTrb});
-				$scope.nuevoTrb = null;
-				//alert($scope.grTrab);
-
-		}
-
-		$scope.eliminarPtrabajo = function (dato) {
-				//var pos = $scope.tareas.indexOf({texto:dato});
-				$scope.grTrab.splice(dato);
+		$scope.agregarPtrabajo = function (_op) {
+		    if(_op === 0){
+        		if ($scope.nuevoTrb != null)
+        				$scope.grTrab.push({trb_name: $scope.nuevoTrb});
+        				$scope.nuevoTrb = null;
+		    }
+		    else{
+		        if(_op === 1){
+		             if ($scope.EditaTrb != null)
+                		$scope.OneProject.grTrb.push({trb_name: $scope.EditaTrb});
+                		$scope.EditaTrb = null;
+		            
+		        }
+		    }
+    	}
+    	
+		$scope.eliminarPtrabajo = function (_op,_index) {
+			if(_op === 0){
+                  if (_index === 0 && $scope.grTrab.length > 0){
+    				    $scope.grTrab.splice(0,1)
+    				}
+    				else{
+    				    $scope.grTrab.splice(_index,1)
+    				}				    
+			}
+			else{
+			    if(_op === 1){
+			        
+			        if (_index === 0 && $scope.OneProject.grTrb.length > 0){
+    				    $scope.OneProject.grTrb.splice(0,1)
+    				}
+    				else{
+    				    $scope.OneProject.grTrb.splice(_index,1)
+    				    
+    				}
+			        
+			    }
+			}
+				
+				
 		}
     
         
         refresh();
         
     });
-
-    
-    
