a552433feb44a6bab27151be7fa2f29d6d9fc7d6
aws1718casm <carlossm50@gmail.com>
2018-05-06 15:30:22 +0000

    Deploy Heroku without authentication

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ app.use(bodyParser.json());
 
 
 app.get(baseAPI + "/projects",
-    passport.authenticate(['basic','localapikey'], { session: false }),
+    //passport.authenticate(['basic','localapikey'], { session: false }),
     (request, response) => {
         console.log("GET /projects");
 
