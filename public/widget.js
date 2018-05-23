d5c91eb83a7da27c3fb30e829143df8f0144893b
aws1718casm <carlossm50@gmail.com>
2018-05-23 17:02:18 +0000

    Widget

--- public/widget.js
+++ public/widget.js
@@ -1,45 +1,75 @@
 var DemoApp = angular.module('DemoApp', ['dx']);
 
-DemoApp.controller('DemoController', function DemoController($scope,$http) {
+DemoApp.controller('DemoController', function DemoController($scope, $http) {
     var dataSource = [];
-    
-    var cant_groups=0;
+
+    var cant_groups = 0;
     var results = [];
     var universitypushed = '';
-    var x=0;
-    function refresh(){
-            $http.get("https://aws1718-03.herokuapp.com/api/v1/groups?apikey=asdfg").then(function (response){
-                $scope.groups = response.data;
-                $scope.filterGroups = response.data;
-                var records = $scope.groups.length ;
-                for( x = 0; x<records; x++){
-                    cant_groups = 0;
-                    if (universitypushed != $scope.groups[x].university){
-                                results = $scope.filterGroups.filter(function (univ) { 
-                                            return univ.university == $scope.groups[x].university;
-                                          });
+    var x = 0;
 
-                                cant_groups = results.length;         
-                                dataSource.push({university: $scope.groups[x].university, cantgrupos: cant_groups});
-                                universitypushed = $scope.groups[x].university; 
+    function refresh() {
+        $http.get("https://aws1718-03.herokuapp.com/api/v1/groups?apikey=asdfg").then(function(response) {
+            $scope.groups = response.data;
+            $scope.filterGroups = response.data;
+            var records = $scope.groups.length;
+            for (x = 0; x < records; x++) {
+                cant_groups = 0;
+                if (universitypushed != $scope.groups[x].university) {
+                    results = $scope.filterGroups.filter(function(univ) {
+                        return univ.university == $scope.groups[x].university;
+                    });
 
-                            /**********************************/
-                                $scope.chartOptions = {
-                                dataSource: dataSource,
-                                series: {
-                                    argumentField: "university",
-                                    valueField: "cantgrupos",
-                                    name: "University",
-                                    type: "bar",
-                                    color: '#ffaa66'
+                    cant_groups = results.length;
+                    dataSource.push({ university: $scope.groups[x].university, cantgrupos: cant_groups });
+                    universitypushed = $scope.groups[x].university;
+
+                    $scope.chartOptions = {
+                        size: {
+                            width: 450
+                        },
+                        palette: "bright",
+                        dataSource: dataSource,
+                        series: [{
+                            argumentField: "university",
+                            valueField: "cantgrupos",
+                            label: {
+                                visible: true,
+                                connector: {
+                                    visible: true,
+                                    width: 1
                                 }
-                            };
-                            /**********************************/
                             }
-                            
-                }
-            })
-        }
+                        }],
+                        title: "Projection of groups by university",
+                        "export": {
+                            enabled: true
+                        },
+                        onPointClick: function(e) {
+                            var point = e.target;
 
-        refresh();
+                            toggleVisibility(point);
+                        },
+                        onLegendClick: function(e) {
+                            var arg = e.target;
+
+                            toggleVisibility(e.component.getAllSeries()[0].getPointsByArg(arg)[0]);
+                        }
+                    };
+
+                    function toggleVisibility(item) {
+                        if (item.isVisible()) {
+                            item.hide();
+                        }
+                        else {
+                            item.show();
+                        }
+                    }
+                }
+
+            }
+        })
+    }
+
+    refresh();
 });
